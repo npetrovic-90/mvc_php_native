e79ba9bcbde9f0559793c15595ed6cfe4c804939
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2022-04-30 13:34:47 +0200

    Setting up Database.php.

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 //Database params
-define('DB_HOST','');
-define('DB_USER','');
+define('DB_HOST','localhost');
+define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','');
+define('DB_NAME','mvc_php_native');
 
 //root of our app
 define('APPROOT',dirname(dirname(__FILE__)));
